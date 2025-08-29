6ec13ff54802ec9e53cbdccd687d9acb57a140fd
Dimqe <petr.makarov249@gmail.com>
2025-08-29 20:23:42 +0200

    hww2

---
+++ app/notes/@modal/(.)[id]/page.tsx
@@ -0,0 +1,19 @@
+import Modal from "@/components/Modal/Modal";
+import { fetchNoteById } from "@/lib/api";
+import NotePreview from "@/components/NotePreview/NotePreview";
+import { notFound } from "next/navigation";
+
+interface NotePageProps {
+  params: { id: string };
+}
+
+export default async function NoteModalPage({ params }: NotePageProps) {
+  const note = await fetchNoteById(params.id);
+  if (!note) return notFound();
+
+  return (
+    <Modal onClose={() => window.history.back()}>
+      <NotePreview note={note} />
+    </Modal>
+  );
+}
