2bc0262aaa2fb43d89f25e79e26ae90d6aa6c962
Dimqe <petr.makarov249@gmail.com>
2025-08-29 22:26:29 +0200

    21322211

---
+++ app/notes/filter/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function FilterLayout({ children }: LayoutProps) {
+  return (
+    <div>
+     
+      {children}
+    </div>
+  );
+}
